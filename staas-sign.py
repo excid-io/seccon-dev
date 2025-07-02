74c6fc91dec5f6edca69a7c40169ea941a117124
lefosg <47362786+lefosg@users.noreply.github.com>
2025-07-02 18:21:25 +0300

    adasdadasdasd

--- staas-sign.py
+++ staas-sign.py
@@ -4,11 +4,12 @@ import requests
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(description="Sign an artifact using STaaS (https://staas.excid.io)")
+parser = argparse.ArgumentParser(description="Sign an artifact using STaaS (https://staas.excid.io)\nA path to an artifact is provided, and its digest is sent to STaaS. STaaS then returns the signature in a bundle.")
 parser.add_argument('-t','--token', type=str, metavar='', required=True, help='Authorization token to access STaaS API')
 parser.add_argument('-a', '--artifact', type=str, metavar='', required=True, help='Path to the artifact to sign')
 parser.add_argument('-c', '--comment', type=str, metavar='', required=False, default='Signing w/ STaaS', help='A comment to accompany the signing (staas-specific info, not related to signature)')
-parser.add_argument('-v', '--verbose', type=bool, metavar='', required=False, default=False, help='Verbose output')
+parser.add_argument('-o', '--output', type=str, metavar='', required=False, default='output.bundle', help='Name output file (default is output.bundle)')
+parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output')
 
 args = parser.parse_args()
 
@@ -42,5 +43,5 @@ if args.verbose:
 print("Signed artifact " + FILENAME)
 print("Response code: " + str(response.status_code))
 
-with open("image_" + FILENAME + ".bundle", "w") as text_file:
+with open(args.output, "w") as text_file:
     text_file.write(response.text)
