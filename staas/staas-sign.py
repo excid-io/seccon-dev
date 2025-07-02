c90f39af02c329df743250be43ccfba1150f55d2
lefosg <47362786+lefosg@users.noreply.github.com>
2025-07-02 17:34:21 +0300

    staas new

---
+++ staas/staas-sign.py
@@ -0,0 +1,46 @@
+import hashlib
+import base64
+import requests
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="Sign an artifact using STaaS (https://staas.excid.io)")
+parser.add_argument('-t','--token', type=str, metavar='', required=True, help='Authorization token to access STaaS API')
+parser.add_argument('-a', '--artifact', type=str, metavar='', required=True, help='Path to the artifact to sign')
+parser.add_argument('-c', '--comment', type=str, metavar='', required=False, default='Signing w/ STaaS', help='A comment to accompany the signing (staas-specific info, not related to signature)')
+parser.add_argument('-v', '--verbose', type=bool, metavar='', required=False, default=False, help='Verbose output')
+
+args = parser.parse_args()
+
+API_TOKEN = args.token  # Authorization token for the second argument
+FILENAME = args.artifact  # File path for the first argument
+COMMENT = args.comment  # Comment
+
+with open(FILENAME,"rb") as f:
+    bytes = f.read() # read entire file as bytes
+    artifact_digest = hashlib.sha256(bytes).digest()
+
+url="https://staas.excid.io/"
+headers = {
+'Content-Type': 'application/json',
+'Authorization': 'Basic ' + API_TOKEN
+}
+
+payload = f"""
+{{
+    "HashBase64":"{base64.b64encode(artifact_digest).decode()}",
+    "Comment":"{COMMENT}"
+}} """
+
+if args.verbose:
+    print(payload)
+
+response = requests.request("POST", url + "Api/Sign", headers=headers, data=payload)
+if args.verbose:
+    print(response.text)
+
+print("Signed artifact " + FILENAME)
+print("Response code: " + str(response.status_code))
+
+with open("image_" + FILENAME + ".bundle", "w") as text_file:
+    text_file.write(response.text)
