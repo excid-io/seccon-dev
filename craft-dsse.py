f8c37e1f7179c1aee936578ea8dc2316c4815c90
lefosg <47362786+lefosg@users.noreply.github.com>
2025-07-04 10:15:10 +0300

    slsa attestation

--- craft-dsse.py
+++ craft-dsse.py
@@ -15,17 +15,18 @@ parser.add_argument('-f','--file', type=str, metavar='', required=True, default=
 parser.add_argument('-p', '--payload', type=str, metavar='', required=True, help='Path to base64 encoded in-toto statement of SBOM or SLSA provenance file')
 parser.add_argument('-s', '--signature', type=str, metavar='', required=True, help='Path to base64 encoded signature')
 
-media_type_group = parser.add_mutually_exclusive_group()
-media_type_group.add_argument("--slsa-provenance", dest="provenance", action="store_true", help="DSSE envelope must include the SLSA provenance media type")
-media_type_group.add_argument("--sbom", action="store_true", help="DSSE envelope must include the SBOM media type")
+# media_type_group = parser.add_mutually_exclusive_group()
+# media_type_group.add_argument("--slsa-provenance", dest="provenance", action="store_true", help="DSSE envelope must include the SLSA provenance media type")
+# media_type_group.add_argument("--sbom", action="store_true", help="DSSE envelope must include the SBOM media type")
 
 args = parser.parse_args()
 
 if not (args.provenance ^ args.sbom):  # XOR operation
     parser.error("Exactly one of --slsa-provenance or --sbom must be provided.")
 
-media_type_slsa = "application/vnd.in-toto.provenance+dsse"
-media_type_sbom = "application/vnd.in-toto+json"
+# media_type_slsa = "application/vnd.in-toto.provenance+dsse"
+# media_type_sbom = "application/vnd.in-toto+json"
+media_type = "application/vnd.in-toto+json"
 
 schemaFile = args.file
 payloadFile = args.payload
@@ -34,10 +35,12 @@ signatureFile = args.signature
 with open(schemaFile, 'r') as dsse_file:
     dsse_data = json.load(dsse_file)
 
-if args.sbom:
-    dsse_data['payloadType'] = media_type_sbom
-elif args.provenance:
-    dsse_data['payloadType'] = media_type_slsa
+# if args.sbom:
+#     dsse_data['payloadType'] = media_type_sbom
+# elif args.provenance:
+#     dsse_data['payloadType'] = media_type_slsa
+
+dsse_data['payloadType'] = media_type
 
 with open(payloadFile, 'r') as payload_file:
     payload_data = payload_file.read()
